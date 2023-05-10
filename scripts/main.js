32aabf9d71a5e91b93ebd8ceaab85d35989f5a8a
astral <117725332+astral0002@users.noreply.github.com>
2023-05-10 10:26:35 +0200

    updated styles and general looks
    updated styles, added a contact-me part of the page
    //the commented out skills section is indev and not available online yet

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+const instagram = () => {
+    var link = "https://www.instagram.com/0002ast";
+    window.open(link);
+}
+const discord = () => {
+    var text = "astral#7180"
+    navigator.clipboard.writeText(text);
+    alert("You have copied the name.")
+}
