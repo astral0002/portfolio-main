585f855359dfeaedd1de843af4c93bd073fef7c1
astral <117725332+astral0002@users.noreply.github.com>
2023-05-23 19:41:48 +0200

    Design and content update
    The website has added some new content, including a somewhat complete about-me section.
    The styling has been adjusted, and the color scheme is to be soon changed.

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,26 @@
+// Greet on enter
+
+window.onload = function greet() {
+    let greets = [
+        "Zdravo!",
+        "Pozdrav!",
+        "Hej!"
+    ];
+    let rand = greets[Math.floor(Math.random() * greets.length)];
+    let element = document.getElementById("pozdrav");
+    element.innerHTML = `👋 ${rand}`
+};
+// social media handles
 const instagram = () => {
-    var link = "https://www.instagram.com/0002ast";
+    var link = "https://www.instagram.com/lukicjovan6/";
     window.open(link);
-}
+};
 const discord = () => {
     var text = "astral#7180"
     navigator.clipboard.writeText(text);
     alert("Copied to clipboard!");
-}
+};
 const github = () => {
     var link = "https://github.com/astral0002";
     window.open(link);
-}
+};
